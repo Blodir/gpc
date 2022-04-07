143ffa1453b317a1498e5acba6514e7b22d30f0c
Make Holopainen <markusp_holopainen@yahoo.com>
2022-04-07 09:59:20 +0300

    Added player model and movement animations to movement test scene

--- Assets/Scripts/PlayerMovement/PlayerMove.cs
+++ Assets/Scripts/PlayerMovement/PlayerMove.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
   //private MovementManager movementManager;
-	public float moveSpeed = 10;
-	public float turnSpeed = 10;
-	private Vector3 move;
-	private Vector3 direction;
+  public float moveSpeed = 10;
+  public float turnSpeed = 10;
+  [SerializeField]
+  private Animator animator;
+  [SerializeField]
+  private float animationDampTime = 0.1f;
+  private Vector3 move;
+  private Vector3 direction;
   private float delta;
 
   private void Start() {
@@ -16,25 +20,32 @@ public class PlayerMove : MonoBehaviour {
 
   void Update() {
     delta = Time.deltaTime;
-		MovementManager.Instance.UpdatePlayerMovement(delta);
-		//movementManager.UpdatePlayerMovement(delta);
-		MovePlayer();
-		TurnPlayer();
+    MovementManager.Instance.UpdatePlayerMovement(delta);
+    //movementManager.UpdatePlayerMovement(delta);
+    MovePlayer();
+    TurnPlayer();
   }
 
-	private void MovePlayer() {
-		//Vector3 move = new Vector3(movementManager.moveX, 0, movementManager.moveZ);
-		Vector3 newMove = new Vector3(MovementManager.Instance.moveX, 0, MovementManager.Instance.moveZ);
-		move = newMove;
-		transform.position += move * moveSpeed;
-	}
+  private void MovePlayer() {
+    //Vector3 move = new Vector3(movementManager.moveX, 0, movementManager.moveZ);
+    move = new Vector3(MovementManager.Instance.moveX, 0, MovementManager.Instance.moveZ);
+    transform.position += move * moveSpeed;
+    if (move.magnitude > 0)
+    {
+      animator.SetFloat("Speed", 1f, animationDampTime, Time.deltaTime);
+    }
+    else
+    {
+      animator.SetFloat("Speed", 0f, animationDampTime, Time.deltaTime);
+    }
+  }
 
-	private void TurnPlayer() {
-		//Vector3 direction = new Vector3(movementManager.moveX, 0, movementManager.moveZ);
-		Vector3 newDirection = new Vector3(MovementManager.Instance.moveX, 0, MovementManager.Instance.moveZ);
-		direction += newDirection;
-		Quaternion rotateToDirection = Quaternion.LookRotation(direction);
-		Quaternion rotate = Quaternion.Lerp(transform.rotation, rotateToDirection, turnSpeed);
-		transform.rotation = rotate;
-	}
+  private void TurnPlayer() {
+    //Vector3 direction = new Vector3(movementManager.moveX, 0, movementManager.moveZ);
+    Vector3 newDirection = new Vector3(MovementManager.Instance.moveX, 0, MovementManager.Instance.moveZ);
+    direction += newDirection;
+    Quaternion rotateToDirection = Quaternion.LookRotation(direction);
+    Quaternion rotate = Quaternion.Lerp(transform.rotation, rotateToDirection, turnSpeed);
+    transform.rotation = rotate;
+  }
 }
